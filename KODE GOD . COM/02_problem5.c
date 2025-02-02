3f418472b5685f0867366af210ff1886941e9fc0
Pratik Vilas Gadhe <pratikgadhe366@gmail.com>
2025-02-02 20:58:16 +0530

    C Language 2025

---
+++ KODE GOD . COM/02_problem5.c
@@ -0,0 +1,13 @@
+//Write a program to accept a number from user and print if it is even or odd in C language
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Enter a Number :");
+    scanf("%d",&n);
+    if(n%2 == 0){
+        printf("%d is Even number\n",n);
+    }else{
+        printf("%d is odd number\n",n);
+    }
+    return 0;
+}
